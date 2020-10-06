62fe2bd6dec84deaed96a321825b987b18b54c27
Dixit <Shashank.Dixit@philips.com>
2020-10-06 14:37:21 +0530

    created project

---
+++ ChatAPI/Controllers/ChatAppController.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ChatAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ChatAppController : ControllerBase
+    {
+        // GET: api/ChatApp
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+       /* // GET: api/ChatApp/5
+        [HttpGet("{id}", Name = "Get")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST: api/ChatApp
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT: api/ChatApp/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE: api/ApiWithActions/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }*/
+    }
+}
---
+++ ChatAPITest/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Xunit;
+
+namespace ChatAPITest
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
