e5f9162729338890214f4f08f2957250af16c0c7
SwadeeshSrivatsaMR <Swadeesh.SrivatsaMR@philips.com>
2020-10-07 21:39:28 +0530

    Added Data Model

--- DataAccessLayer/Users.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace DataAccessLayer
-{
-    public class Users
-    {
-        public string Name { get; set; }
-        public string Email { get; set; }
-        public string Password { get; set; }
-    }
-}
