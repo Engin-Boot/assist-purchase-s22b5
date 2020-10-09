37b8e7c146e9c66b9720ffd2b7978aa9c4e00be3
Swadeesh Srivatsa MR <Swadeesh.SrivatsaMR@philips.com>
2020-10-09 18:19:20 +0530

    Modified DataAccessLayerTest

--- DataAccessLayerTest/ProductManagementMemoryDbTest.cs
+++ DataAccessLayerTest/ProductManagementMemoryDbTest.cs
@@ -3,6 +3,7 @@ using System.Linq;
 using DataAccessLayer;
 using DataAccessLayer.Utils;
 using Xunit;
+using DataModel;
 namespace DataAccessLayerTest
 {
     public class ProductManagementMemoryDbTest
@@ -110,6 +111,44 @@ namespace DataAccessLayerTest
         {
             var productList = _productManagement.GetAllProducts(_transactionManager);
             Assert.True(productList.Any());
+        }
+
+        [Fact]
+        public void TestToStringData()
+        {
+            var testProd = new ProductDataModel
+            {
+                ProductName = "IntelliVue X3",
+                Id = 104,
+                ProductSeries = "Intellivue",
+                ProductModel = "X3",
+                ProductPrice = 1000000,
+                ProductWeight = 1000,
+                Portable = true,
+                MonitorResolution = "1024*720",
+                Measurement = new List<string>()
+                {
+                    "SPO2", "ECG"
+                }
+            };
+            Assert.False(string.IsNullOrEmpty(testProd.ToString()));
+            testProd = new ProductDataModel
+            {
+                ProductName = "",
+                Id = 104,
+                ProductSeries = "Intellivue",
+                ProductModel = "X3",
+                ProductPrice = 1000000,
+                ProductWeight = 1000,
+                Portable = true,
+                MonitorResolution = "1024*720",
+                Measurement = new List<string>()
+                {
+                    "SPO2", "ECG"
+                }
+            };
+            Assert.True(string.IsNullOrEmpty(testProd.ProductName));
+
         }
     }
 }
