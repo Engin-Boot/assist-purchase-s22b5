2a146173110cbd60248240e05a3d6a8e0c73ba1c
Swadeesh Srivatsa MR <Swadeesh.SrivatsaMR@philips.com>
2020-10-09 18:10:47 +0530

    Modified DataAccessLayer

--- DataAccessLayer/IProductManagement.cs
+++ DataAccessLayer/IProductManagement.cs
@@ -1,14 +1,13 @@
-﻿using DataModel;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using DataAccessLayer.Utils;
 
 namespace DataAccessLayer
 {
     public interface IProductManagement
     {
-        bool AddProduct(ProductDataModel product, ITransactionManager manager);
-        bool RemoveProduct(ProductDataModel product, ITransactionManager manager);
-        IEnumerable<ProductDataModel> GetAllProducts(ITransactionManager manager);
-        bool UpdateProduct(ProductDataModel product, ITransactionManager manager);
+        bool AddProduct(DataModel.ProductDataModel product, ITransactionManager manager);
+        bool RemoveProduct(DataModel.ProductDataModel product, ITransactionManager manager);
+        IEnumerable<DataModel.ProductDataModel> GetAllProducts(ITransactionManager manager);
+        bool UpdateProduct(DataModel.ProductDataModel product, ITransactionManager manager);
     }
 }
