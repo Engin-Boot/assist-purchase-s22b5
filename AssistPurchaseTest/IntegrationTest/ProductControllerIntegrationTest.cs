0426a5270f33af0d9e6514264d3d1cde6a9b83c4
Swadeesh Srivatsa MR <Swadeesh.SrivatsaMR@philips.com>
2020-10-14 19:48:33 +0530

    added test case in ProductControllerIntegrationTest.cs

--- AssistPurchaseTest/IntegrationTest/ProductControllerIntegrationTest.cs
+++ AssistPurchaseTest/IntegrationTest/ProductControllerIntegrationTest.cs
@@ -1,5 +1,6 @@
 ﻿using Xunit;
 using System.Collections.Generic;
+using System.Net;
 using DataModel;
 using RestSharp;
 using RestSharp.Serialization.Json;
@@ -15,7 +16,7 @@ namespace AssistPurchaseTest.IntegrationTest
             _request = new RestRequest("product/");
         }
         [Fact]
-        public void TestAddData()
+        public void TestGetData()
         {
             
             var data = _restClient.Get(_request);
@@ -26,5 +27,35 @@ namespace AssistPurchaseTest.IntegrationTest
             Assert.True(output[1].Id == 102);
             Assert.True(output[0].Portable);
         }
+
+        [Fact]
+        public void TestAddData()
+        {
+            var testProductDataModel = new ProductDataModel
+            {
+                ProductName = "IntelliVue X3",
+                Id = 110,
+                ProductSeries = "Intellivue",
+                ProductModel = "X3",
+                Weight = 1000,
+                Portable = true,
+                MonitorResolution = "1024*720",
+                ScreenSize = 5,
+                Measurement = new List<string>()
+                {
+                    "SPO2", "ECG"
+                }
+            };
+            _request.AddHeader("Content-Type", "application/json; charset=utf-8");
+            _request.AddJsonBody(testProductDataModel);
+            var data = _restClient.Post(_request);
+
+            var deserialize = new JsonDeserializer();
+            var output = deserialize.Deserialize<HttpStatusCode>(data);
+
+            Assert.Equal(HttpStatusCode.OK, output);
+
+        }
+
     }
 }
